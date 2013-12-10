445e0283a14c99db42e4efca7a2edc9c47ff6772
jonwa <b12jonwa@SD20309.hs.local>
2013-12-10 09:56:04 +0100

    erased that mf

--- TravelingSalesman/TravelingSalesman/NearestNeighbour.h
+++ TravelingSalesman/TravelingSalesman/NearestNeighbour.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Graph.h"
+#include "Path.h"
 #include <vector>
 #include <numeric>
 
@@ -7,27 +8,10 @@ namespace tsp
 {
 	namespace algorithm
 	{
-		template <class T, template <class> class MatrixRep, template <class, template <class> class > class AccessorPolicy> 
-		void NearestNeighbour(int start_node, int end_node, const Graph<T, MatrixRep, AccessorPolicy>& graph)
+		template <class T, template <class> class MatrixRep, template <class, template <class> class > class AccessorPolicy>
+		void NearestNeighbour(const Graph<T, MatrixRep, AccessorPolicy>& graph)
 		{
-			int lowest_weight_found = std::numeric_limits<T>::max();
-			int next_node;  
-			int current_node = start_node; 
-			std::vector<int> visited; 
-
-			while(start_node != end_node)
-			{
-				for(auto node : graph.get_neighbours(current_node))
-				{
-					if(graph.get_weight(node.start_node, node.neighbour) < lowest_weight_found)
-					{
-						lowest_weight_found = graph.get_weight(node.start_node, node.neighbour);
-						next_node = node; 
-					}
-				}
-				current_node = next_node; 
-				visited.push_back(current_node);
-			}
+			
 		}
 	}
 }
